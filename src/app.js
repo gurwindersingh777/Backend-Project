26933b6f48bc37104e44cdef5f6e70718deed393
Gurwinder Singh <singhgurwinder2004@gmail.com>
2026-01-23 12:58:10 +0530

    add user controller and some changes

--- src/app.js
+++ src/app.js
@@ -8,13 +8,15 @@ app.use(cors({ origin: process.env.CORS_PATH, credentials: true }));
 app.use(cookieParser())
 
 // Commer middlewares
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static("public"));
 
 // Routes
-import healthcheckRouter from './routes/healthcheck.routes.js';
-app.use("/api/v1/healthcheck",healthcheckRouter)
+import userRouter from './routes/user.routes.js';
 
+// Calling Routes
+app.use("/api/v1/users",userRouter); // https://localhost:8000/api/v1/users/register
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
 export  { app };
